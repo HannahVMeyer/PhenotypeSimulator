6d276e244df35b478a94088ac3c322c4f62933db
HannahVMeyer <hannah.v.meyer@googlemail.com>
2017-05-19 09:28:32 +0100

    Add writing output as plink via package snpStats

--- R/PhenotypeSimulator.R
+++ R/PhenotypeSimulator.R
@@ -3,10 +3,13 @@
 #'
 #' @docType package
 #' @name PhenotypeSimulator
-#' @importFrom methods getPackageName
+#' @import snpStats
+#' @importFrom methods getPackageName as
 #' @importFrom stats median pnorm rbinom rnorm runif sd var na.omit
-#' @importFrom utils read.table write.table
+#' @importFrom utils read.table write.table 
 #' @importFrom optparse make_option parse_args OptionParser
 #' @importFrom plyr l_ply
+#' @importFrom snpStats write.plink
+#' @importClassesFrom snpStats SnpMatrix
 #' @importFrom R.utils countLines
 NULL
