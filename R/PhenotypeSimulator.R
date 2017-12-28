5b752378d209accb38dd02f1b6f4536d790273e8
HannahVMeyer <hannah.v.meyer@googlemail.com>
2017-12-28 11:05:11 +0100

    Add rexp import for simulation of effect size distributions

--- R/PhenotypeSimulator.R
+++ R/PhenotypeSimulator.R
@@ -5,7 +5,7 @@
 #' @name PhenotypeSimulator
 #' @import snpStats
 #' @importFrom methods getPackageName as
-#' @importFrom stats median pnorm rbinom rnorm runif sd var na.omit
+#' @importFrom stats median pnorm rbinom rnorm runif sd var na.omit rexp
 #' @importFrom utils read.table write.table 
 #' @importFrom optparse make_option parse_args OptionParser
 #' @importFrom plyr l_ply
